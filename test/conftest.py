63ba898624aa0fba9e8439cd5f6e443ec6746a8f
Rafal <stapinskirafal@gmail.com>
2022-04-10 18:01:03 -0400

    🧪 Dynamic path to redis-server

--- test/conftest.py
+++ test/conftest.py
@@ -1,4 +1,6 @@
+import shutil
+
 from pytest_redis import factories
 
-redis_proc = factories.redis_proc(executable="/opt/homebrew/bin/redis-server", port=None)
+redis_proc = factories.redis_proc(executable=shutil.which("redis-server"), port=1234)
 redis = factories.redisdb("redis_proc")
