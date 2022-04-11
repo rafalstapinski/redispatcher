0d8263cb920b23f07e7b2014e333bbf317c96e47
Rafal <stapinskirafal@gmail.com>
2022-04-10 21:07:57 -0400

    🕵️ Look into redis-server in github actions

--- test/conftest.py
+++ test/conftest.py
@@ -2,5 +2,7 @@ import shutil
 
 from pytest_redis import factories
 
+print(f"\n\n\n{shutil.which('redis-server')=}\n\n\n")
+
 redis_proc = factories.redis_proc(executable=shutil.which("redis-server"), port=1234)
 redis = factories.redisdb("redis_proc")
